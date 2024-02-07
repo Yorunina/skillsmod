354c35ce115ce53148d436a991cf464024f89455
Pufferfish <puffish@puffish.net>
2024-02-07 11:51:28 +0100

    Keep category in sync after showing or hiding

--- Common/src/main/java/net/puffish/skillsmod/client/SkillsClientMod.java
+++ Common/src/main/java/net/puffish/skillsmod/client/SkillsClientMod.java
@@ -100,9 +100,7 @@ public class SkillsClientMod {
 	}
 
 	private void onOpenKeyPress() {
-		MinecraftClient.getInstance().setScreen(new SkillsScreen(
-				categories.values().stream().toList()
-		));
+		MinecraftClient.getInstance().setScreen(new SkillsScreen(categories));
 	}
 
 	private void onShowCategory(ShowCategoryInPacket packet) {
