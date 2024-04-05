bc53bbdfe25b15affc96e907a92d379c778d5766
Pufferfish <puffish@puffish.net>
2024-04-05 15:12:35 +0200

    Fix compatibility

--- Common/src/main/java/net/puffish/skillsmod/access/BufferBuilderAccess.java
+++
@@ -1,9 +0,0 @@
-package net.puffish.skillsmod.access;
-
-import net.minecraft.util.math.Matrix4f;
-
-import java.util.List;
-
-public interface BufferBuilderAccess {
-	void setEmits(List<Matrix4f> emits);
-}
