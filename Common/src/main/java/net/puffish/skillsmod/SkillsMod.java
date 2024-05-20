9a326eaf6f2ea5d81933a0224c83aa98e85b5a98
Pufferfish <puffish@puffish.net>
2024-05-20 17:51:16 +0200

    Add scoreboard and tag operations

--- Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
+++ Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
@@ -23,9 +23,11 @@ import net.puffish.skillsmod.calculation.operation.builtin.EffectOperation;
 import net.puffish.skillsmod.calculation.operation.builtin.EntityTypeCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.ItemCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.ItemStackCondition;
+import net.puffish.skillsmod.calculation.operation.builtin.ScoreboardOperation;
 import net.puffish.skillsmod.calculation.operation.builtin.StatCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.StatTypeCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.SwitchOperation;
+import net.puffish.skillsmod.calculation.operation.builtin.TagCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.legacy.LegacyBlockTagCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.legacy.LegacyEntityTypeTagCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.legacy.LegacyItemTagCondition;
@@ -168,9 +170,11 @@ public class SkillsMod {
 		EntityTypeCondition.register();
 		ItemCondition.register();
 		ItemStackCondition.register();
+		ScoreboardOperation.register();
 		StatCondition.register();
 		StatTypeCondition.register();
 		SwitchOperation.register();
+		TagCondition.register();
 
 		CraftItemExperienceSource.register();
 		EatFoodExperienceSource.register();
