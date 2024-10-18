f23b29d51498f39e85bef90a2816e1cb9623716d
Pufferfish <puffish@puffish.net>
2024-10-18 12:12:47 +0200

    Disallow legacy formats in new config versions

--- Common/src/main/java/net/puffish/skillsmod/impl/rewards/RewardConfigContextImpl.java
+++ Common/src/main/java/net/puffish/skillsmod/impl/rewards/RewardConfigContextImpl.java
@@ -7,11 +7,12 @@ import net.puffish.skillsmod.api.json.JsonElement;
 import net.puffish.skillsmod.api.reward.RewardConfigContext;
 import net.puffish.skillsmod.api.util.Problem;
 import net.puffish.skillsmod.api.util.Result;
+import net.puffish.skillsmod.util.VersionContext;
 
 public record RewardConfigContextImpl(
 		ConfigContext context,
 		Result<JsonElement, Problem> maybeDataElement
-) implements RewardConfigContext {
+) implements RewardConfigContext, VersionContext {
 
 	@Override
 	public MinecraftServer getServer() {
@@ -26,5 +27,13 @@ public record RewardConfigContextImpl(
 	@Override
 	public Result<JsonElement, Problem> getData() {
 		return maybeDataElement;
+	}
+
+	@Override
+	public int getVersion() {
+		if (context instanceof VersionContext versionContext) {
+			return versionContext.getVersion();
+		}
+		return Integer.MIN_VALUE;
 	}
 }
